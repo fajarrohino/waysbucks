e3da7fb022be1b506e584f2c1956904042b3e557
eannoy <fajarrohino14@gmial.com>
2023-11-03 01:12:48 +0700

    first commit react mui

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import { Box, Typography } from "@mui/material";
+import Navbar from "../components/Navbar";
+
+export default function Home() {
+  return (
+    <>
+      <Box>
+        <Typography>This is home pages</Typography>
+        <Navbar />
+      </Box>
+    </>
+  );
+}
