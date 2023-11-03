05ed5721f13ad9e814bea1255d60b2be7a389d57
eannoy <fajarrohino14@gmial.com>
2023-11-04 01:45:07 +0700

    style: hover

--- client/src/components/Jumbotron.tsx
+++ client/src/components/Jumbotron.tsx
@@ -15,13 +15,27 @@ export default function Jumbotron() {
             </Typography>
             <Typography pt={3}>Let's Order...</Typography>
           </Box>
-          <Box position={"absolute"} right={100}>
+          <Box
+            position={"absolute"}
+            right={100}
+            sx={{
+              transition: "transform 0.3s ease",
+              ":hover": {
+                transform: "scale(1.1)",
+              },
+            }}
+          >
             <img
               src="https://s3-alpha-sig.figma.com/img/f030/f66f/4fe88ea0871b9f90f3bd425e6abf5f58?Expires=1699833600&Signature=JOC6WaFAWvxSbXjVzi3j2MZ3x93zDWtEIFcHmimOxYCF~2TtF1ruaNKFlPuKrJMoKa~AvI5W2nns9WDNfAVY2Y27GqhxhqTCcHcvtFs-O4XuUCzxNZLNgHmJNYeozR8B4BfrrOCCvnU-JZ3vgiN0AdN14-ZWXgqKmQWs9ICJmVYxjEb~nG5mdl0CorjAqpkMz~44Fe5zx8kJR7Eu2s2qciOr7l4x1Effj5f6OzzkiiHmLv39lUe1HPT6qXR2na3unwvfLafX7RnFvEzg2VtWsQQwTapyt00YaXssisoEh0k8bqwUrQr7k9bA05akF-IH2t79enxTa6fz4uwU3DYR1g__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
               alt="waysbuck"
               width={"451px"}
               height={"324px"}
-              style={{ objectFit: "cover", borderStartEndRadius: "10px", borderEndEndRadius: "10px" }}
+              style={{
+                objectFit: "cover",
+                borderStartEndRadius: "10px",
+                borderEndEndRadius: "10px",
+                transition: "transform 0.3s ease",
+              }}
             />
           </Box>
         </Box>
--- client/src/components/card/CardOrder.tsx
+++ client/src/components/card/CardOrder.tsx
@@ -9,7 +9,15 @@ export default function CardOrder() {
           height="312"
           image="https://s3-alpha-sig.figma.com/img/4348/8c71/4273019eb029d3a34583371f7000ecba?Expires=1699833600&Signature=A7zFnStM~2lnmMkio-~eYdJOpVUdHLp~D3coUjapbyB2e0~XVVevlnaW2QSI3hTPZAf0ejDcbS0-i0O4~A2TFjvYCMQVWgjWF8i1IVExENDBHyYDG5ZtbkO5-PNeJ9rPQUkjOhF5YqM2ks~XzVaZkPcqXT7dVyuhskiUdQEKBFkcohUL37zHWr1bFcASxtc4waUPWEfgNLTM5ceo5FKeyoWNTYyiOn9AObptMBSBusHtjdmLyjaNI~xCAtTbGTAxwOnuO3fvNORw0D7xkpn20j6LxQrDlNuCwKHUu7kKLG-KlZPbufa6rHdmrtV8jgJnfzE-XR2d--UHc-ppkGMlow__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
           alt="green iguana"
-          sx={{ borderBottomLeftRadius: 5, borderBottomRightRadius: 5, objectFit: "cover" }}
+          sx={{
+            borderBottomLeftRadius: 5,
+            borderBottomRightRadius: 5,
+            objectFit: "cover",
+            transition: "transform 0.3s ease",
+            ":hover": {
+              transform: "scale(1.1)",
+            },
+          }}
         />
         <CardContent>
           <Typography sx={{ fontSize: "18px", fontWeight: "900" }} color={"#BD0707"}>
