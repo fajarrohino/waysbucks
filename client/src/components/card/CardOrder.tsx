2e763de05a83efc975b98d139775f188d271930b
eannoy <fajarrohino14@gmial.com>
2023-11-13 15:39:18 +0700

    link

--- client/src/components/card/CardOrder.tsx
+++ client/src/components/card/CardOrder.tsx
@@ -4,9 +4,11 @@ import { IOrder } from "../../libs/interface/order";
 
 export default function CardOrder(props: Readonly<IOrder>) {
   return (
-    <Card sx={{ maxWidth: "241px", maxHeight: "392px", m: "10px" }}>
+    <Card sx={{ maxWidth: "241px", maxHeight: "392px", mb: "10px" }}>
       <CardActionArea sx={{ backgroundColor: "#F6DADA" }}>
-        <CardMedia component={"img"} image={props.picture} sx={{ borderRadius: "10px", objectFit: "cover", width: "241px", height: "312px" }} />
+        <Link to={`/order/${props.id}`} style={{ textDecoration: "none" }}>
+          <CardMedia component={"img"} image={props.picture} sx={{ borderRadius: "10px", objectFit: "cover", width: "241px", height: "312px" }} />
+        </Link>
         <CardContent>
           <Link to={`/order/${props.id}`} style={{ textDecoration: "none" }}>
             <Typography sx={{ fontSize: "18px", fontWeight: "900" }} color={"#BD0707"}>
